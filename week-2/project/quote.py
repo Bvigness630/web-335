db5de25b9cace3d0848417ca57d2c2cdb048457e
Boyd Vigness <vignessboyd@gmail.com>
2026-06-14 17:24:15 -0500

    week 2 python

---
+++ week-2/project/quote.py
@@ -0,0 +1,12 @@
+"""
+Author: Boyd Vigness
+Date: June 14, 2026
+File Name: quote.py
+Description: This program prints an inspirational quote and credits the original author.
+"""
+
+# Print the quote
+print('"Sometimes it is better to lose and do the right thing than to win and do the wrong thing."')
+
+# Print the author of the quote
+print("- Tony Blair")
